577b29a7c640e10e704f0fde83ac468d6d2f70c2
Valeria <valeria050603@hotmail.com>
2023-06-03 12:33:33 -0600

    main

---
+++ Ejemplo/Videos/main.js
@@ -0,0 +1,27 @@
+//Variables 
+edad = 15; // Tipo numérico
+nombre = "Santo"; //Tipo texto
+cumple = true; //Tipo boleano (false | true)
+
+//console.log()
+//alert()
+
+if (cumple == true){
+    console.log("Feliz cumple");
+    if (edad > 10){
+        console.log("Ya estás grande");
+    }
+}
+
+if (edad > 5){
+    console.log("Eres mayor a 5");
+}
+
+if (nombre == "Santo"){
+    console.log("Hola Santo");
+}
+
+if(cumple == false){
+    cumple = !cumple;
+}
+console.log(cumple);
